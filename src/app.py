c09019b9003c4d7534b460cb1f8eec9c22675674
kaoss <swojak.a@gmail.com>
2019-04-14 19:56:49 +0200

    minor hotfix

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
+
 from flask import Flask
 
 app = Flask(__name__)
+# app.debug = True
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:darkcyan;'>Hellix There!</h1>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
