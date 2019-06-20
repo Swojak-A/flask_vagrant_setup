c0790dbe2c76a2a6a09ec5b3beb9d17f11f5822d
kaoss <swojak.a@gmail.com>
2019-06-20 16:49:04 +0200

    feat: rename path to file from src to app

--- app/app.py
+++ app/app.py
@@ -10,4 +10,4 @@ def hello():
     return "<h1 style='color:darkcyan;'>Hellix There!</h1>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
